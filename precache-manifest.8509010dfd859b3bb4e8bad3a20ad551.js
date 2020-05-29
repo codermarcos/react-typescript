6b24f5ef05d3855851f288c700f3f30747d4db7b
codermarcos <codermarcos@users.noreply.github.com>
2020-05-29 03:13:26 +0000

    deploy: 56eac9213ddbaad3359ce9b6a8b31bbb0064c8d2

---
+++ precache-manifest.8509010dfd859b3bb4e8bad3a20ad551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46cccba55c02620fa1e1f34d841f5eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2927a2e73b2e099b137",
+    "url": "/static/css/main.c48920c2.chunk.css"
+  },
+  {
+    "revision": "aa3ee6a0402bce0966ba",
+    "url": "/static/js/2.c5239f01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c5239f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2927a2e73b2e099b137",
+    "url": "/static/js/main.7d03a363.chunk.js"
+  },
+  {
+    "revision": "8362654ba47ab209cc52",
+    "url": "/static/js/runtime-main.894ef251.js"
+  }
+]);
