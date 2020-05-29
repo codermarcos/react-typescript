dba89031f05cb2b99cf4a2097bc17f9ac2a65545
codermarcos <coder.marcos@gmail.com>
2020-05-29 01:56:48 -0300

    :bug: fix error path

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -6,8 +6,8 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import createRootReducer from 'src/store';
 import rootSaga from 'src/store/saga';
 
-export const history = createBrowserHistory();
 export const sagaMiddleware = createSagaMiddleware();
+export const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
 
 export default function configureStore(preloadedState?: any) {
   const composeEnhancer: typeof compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
